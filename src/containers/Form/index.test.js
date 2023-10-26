5957bf6bcac8ae07af08d9cdf95b20aaf23f858a
shenzor <toppan.frederic@gmail.com>
2023-10-26 10:15:16 +0200

    test form OK

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Form is created", () => {
@@ -11,6 +11,8 @@ describe("When Form is created", () => {
     await screen.findByText("Personnel / Entreprise");
     await screen.findByText("Email");
     await screen.findByTestId("button-test-id");
+    await screen.findByText("Envoyer"); // Vérifie que le texte "Envoyer" est présent initialement.
+  });
   });
 
   describe("and a click is triggered on the submit button", () => {
@@ -24,9 +26,19 @@ describe("When Form is created", () => {
           bubbles: true,
         })
       );
+
+      // Attend que le texte "En cours" apparaisse après le clic.
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+
+      // Vérifie que le texte "Envoyer" n'est plus présent.
+      expect(screen.queryByText("Envoyer")).toBeNull();
+
+      // Attend que le texte "Envoyer" réapparaisse après que le formulaire ait été envoyé.
+      await waitFor(() => {
+        expect(screen.queryByText("En cours")).toBeNull();
+        expect(screen.queryByText("Envoyer")).not.toBeNull();
+      });
+
       expect(onSuccess).toHaveBeenCalled();
     });
   });
-});
